be135a5226ff7c98f1b6f7db2b89eab561770bfb
sophie <s.ischenko1990@gmail.com>
2023-05-04 11:24:11 +0200

    calculatorMock

--- src/calculatorMock.js
+++ src/calculatorMock.js
@@ -1,9 +1,6 @@
-const calculatorMock = {
-    add: jest.fn((a, b) => a + b),
-    subtract: jest.fn((a, b) => a - b),
-    multiply: jest.fn((a, b) => a * b),
-    divide: jest.fn((a, b) => (b === 0 ? NaN : a / b)),
+export const calculatorMock = {
+    add: jest.fn(),
+    subtract: jest.fn(),
+    divide: jest.fn(),
+    multiply: jest.fn(),
   };
-  
-export default calculatorMock;
-  
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,64 +1,58 @@
-jest.mock("../src/calculator", () => ({
-    add: jest.fn(),
-    subtract: jest.fn(),
-    multiply: jest.fn(),
-    divide: jest.fn()
-  }));
-  
-  const calculator = require("../src/calculator");
-  
-  test ("calculator object exists", () => {
-      expect(typeof calculator).toEqual("object");
-  }
-  );
-  
-  test ("calculator has add method", () => {
-      expect(typeof calculator.add).toEqual("function");
-  }
-  );
-  
-  test ("calculator has subtract method", () => {
-      expect(typeof calculator.subtract).toEqual("function");
-  }
-  );
-  
-  test ("calculator has multiply method", () => {
-      expect(typeof calculator.multiply).toEqual("function");
-  }
-  );
-  
-  test ("calculator has divide method", () => {
-      expect(typeof calculator.divide).toEqual("function");
-  }
-  );
-  
-  test ("calculator adds two numbers", () => {
-      calculator.add.mockReturnValue(5);
-      expect(calculator.add(2, 3)).toEqual(5);
-  }
-  );
-  
-  test ("calculator subtracts two numbers", () => {
-      calculator.subtract.mockReturnValue(3);
-      expect(calculator.subtract(5, 2)).toEqual(3);
-  }
-  );
-  
-  test ("calculator divides two numbers", () => {
-      calculator.divide.mockReturnValue(5);
-      expect(calculator.divide(10, 2)).toEqual(5);
-  }
-  );
-  
-  test ("calculator multiplies two numbers", () => {
-      calculator.multiply.mockReturnValue(24);
-      expect(calculator.multiply(4, 6)).toEqual(24);
-  }
-  );
-  
-  test ("calculator divides by zero", () => {
-      calculator.divide.mockImplementation(() => { throw new Error("Cannot divide by zero") });
-      expect(() => calculator.divide(10, 0)).toThrow("Cannot divide by zero");
-  }
-  );
-  
+import { calculatorMock } from "../src/calculatorMock.js";
+
+
+
+test("calculator object exists", () => {
+    expect(typeof calculatorMock).toEqual("object");
+}
+);
+
+test("calculator has add method", () => {
+    expect(typeof calculatorMock.add).toEqual("function");
+}
+);
+
+test("calculator has subtract method", () => {
+    expect(typeof calculatorMock.subtract).toEqual("function");
+}
+);
+
+test("calculator has multiply method", () => {
+    expect(typeof calculatorMock.multiply).toEqual("function");
+}
+);
+
+test("calculator has divide method", () => {
+    expect(typeof calculatorMock.divide).toEqual("function");
+}
+);
+
+test("calculator adds two numbers", () => {
+    calculatorMock.add.mockReturnValue(5);
+    expect(calculatorMock.add(2, 3)).toEqual(5);
+}
+);
+
+test("calculator subtracts two numbers", () => {
+    calculatorMock.subtract.mockReturnValue(3);
+    expect(calculatorMock.subtract(5, 2)).toEqual(3);
+}
+);
+
+test("calculator divides two numbers", () => {
+    calculatorMock.divide.mockReturnValue(5);
+    expect(calculatorMock.divide(10, 2)).toEqual(5);
+}
+);
+
+test("calculator multiplies two numbers", () => {
+    calculatorMock.multiply.mockReturnValue(24);
+    expect(calculatorMock.multiply(4, 6)).toEqual(24);
+}
+);
+
+test("calculator divides by zero", () => {
+    calculatorMock.divide.mockImplementation(() => { throw new Error("Cannot divide by zero") });
+    expect(() => calculatorMock.divide(10, 0)).toThrow("Cannot divide by zero");
+}
+);
